5afbfb0362dcff4ad7138937f02e8ef40d8bc34a
zjosua <zjosua@hotmail.com>
2019-04-18 22:44:23 +0200

    add add-on config

--- __init__.py
+++ __init__.py
@@ -1,21 +1,29 @@
 # Austin Hasten
 # Initial Commit - January 13th, 2018
+# License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
 
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
 
+from .config import local_conf
+
 def newShortcutKeys(self, _old):
     return _old(self) + [
-        ("j", lambda: self._answerCard(1)),
-        ("k", lambda: self._answerCard(2)),
-        ("l", lambda: self._answerCard(3)),
-        (";", lambda: self._answerCard(4)),
+        (local_conf["ans_1"], lambda: self._answerCard(1)),
+        (local_conf["ans_2"], lambda: self._answerCard(2)),
+        (local_conf["ans_3"], lambda: self._answerCard(3)),
+        (local_conf["ans_4"], lambda: self._answerCard(4)),
     ]
 
 def newAnswerCard(self, ease, _old):
-    if self.state == "question":
-        self.state = 'answer'
-    _old(self, min(self.mw.col.sched.answerButtons(self.card), ease))
+    if local_conf["silly_mode"]:
+        if self.state == "question":
+            self.state = 'answer'
+    if local_conf["conflate_ease"]:
+        _old(self, min(self.mw.col.sched.answerButtons(self.card), ease))
+    else:
+        _old(self, ease)
 
 Reviewer._shortcutKeys = wrap(Reviewer._shortcutKeys, newShortcutKeys, "around")
-Reviewer._answerCard = wrap(Reviewer._answerCard, newAnswerCard, "around")
+if local_conf["silly_mode"] or local_conf["conflate_ease"]:
+    Reviewer._answerCard = wrap(Reviewer._answerCard, newAnswerCard, "around")
